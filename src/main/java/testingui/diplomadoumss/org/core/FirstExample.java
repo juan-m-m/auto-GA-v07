bd3e29795b2b7562e4ed782cce27ab47c0b887f0
juan-m-m <juan-es20@hotmail.com>
2019-07-11 23:53:08 -0400

    updated of docent

--- src/main/java/testingui/diplomadoumss/org/core/FirstExample.java
+++ src/main/java/testingui/diplomadoumss/org/core/FirstExample.java
@@ -1,5 +1,7 @@
 package testingui.diplomadoumss.org.core;
 
+import org.openqa.selenium.WebDriver;
+import testingui.diplomadoumss.org.managepage.login.Login;
 import testingui.diplomadoumss.org.utilsfiles.PropertyAccesor;
 
 import java.util.HashMap;
@@ -8,15 +10,13 @@ import java.util.Map;
 public class FirstExample {
     public static void main(String[] args) {
 
-        BroserType browserType = BroserType.valueOf(PropertyAccesor.getInstance().getBrowser());
-        Browser browser = DriverFactory.getInstance().getBrowser(browserType);
-        browser.getWebDriver().get(PropertyAccesor.getInstance().getURL());
-        browser.getWebDriver().quit();
-
-//        exampleMap();
+        WebDriver webDriver = DriverManager.getInstance().getWebDriver();
+        webDriver.get(PropertyAccesor.getInstance().getURL());
+        webDriver.quit();
+        //BasePage basePage = new BasePage();
     }
 
-    public static void exampleMap(){
+    public static void exampleMap() {
         Map<String, String> students = new HashMap<>();
         students.put("101", "nombre 1");
         students.put("102", "nombre 2");
